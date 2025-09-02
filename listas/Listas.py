f9318d303a69fe6e58226412527669a266254a1b
Asael312 <AL07135560@tecmilenio.mx>
2025-09-02 12:47:15 -0600

    Agregue un pequeño progrma de comparador

--- listas/Listas.py
+++ listas/Listas.py
@@ -4,8 +4,10 @@ print(Materias[-1])
 Nombres=[]
 for i in range(5):
     Nombres.append(input("Ingrese el nombre del estudiante: "))
+    break
 for n in range(-1,-6,-1):
     print(Nombres[n])
+    break
 print(Nombres[::-1])
 Temperaturas=[20,30,80,32,51]
 Suma=0
@@ -21,3 +23,14 @@ for a in Temperaturas:
     cont+=1
 print(suma/cont)
 print('Promedio: ', sum(Temperaturas)/len(Temperaturas))
+
+drawn = [5, 11, 9, 42, 3, 49]
+bets = [3, 7, 11, 42, 34, 49]
+hits = 0
+
+for number in bets:
+    if number in drawn:
+        hits += 1
+    breakpoint()
+print(hits)
+
